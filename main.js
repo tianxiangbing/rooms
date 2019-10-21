a6cedb5e23d43f655deeb8e6253d46ece3477d3c
tianxiangbing <55342775@qq.com>
2019-10-21 18:12:27 +0800

    rebots

--- main.js
+++ main.js
@@ -1,31 +1,58 @@
 /**
  * websocket
  */
-const {User,Rooms}  = require('./index')
+const { User, Rooms } = require('./index')
+const { MSG, actionType } = require('./message');
 const server = require('http').createServer();
 const io = require('socket.io')(server);
-
+let rebots = {
+}
 //大厅
 io.on('connection', client => {
     let user = new User();
-    client.emit('user',user);
+    client.emit('user', user);
     client.on('join', data => {
         /* 加入某个房间 */
-        Rooms.join(data,user,io,client)
+        Rooms.join(data, user, io, client)
     });
-    client.on('message',msg=>{
-        if(user.roomId){
+    //同步机器人
+    client.on('rebots', msg => {
+        if (msg.type == actionType.addRebots) {
+            let roomId = user.roomId;
+            rebots[roomId] = msg.body;
+        }
+        //获取当前房间的机器人信息
+        if (msg.type == actionType.getRebots) {
+            let roomId = user.roomId;
+            let rs = rebots[roomId];
+            let msg = new MSG(actionType.getRebots, {
+                rebots: rs
+            })
+            Rooms.send(roomId, rs);
+        }
+        if (msg.type == actionType.updateRebots) {
+            let roomId = user.roomId;
+            rebots[roomId] = msg.body;
+            let rs = rebots[roomId];
+            let msg = new MSG(actionType.getRebots, {
+                rebots: rs
+            })
+            Rooms.send(roomId, rs);
+        }
+    });
+    client.on('message', msg => {
+        if (user.roomId) {
             // io.to(user.roomId).emit('message',msg)
-            if(msg.type == 'update'){
+            if (msg.type == 'update') {
                 user.update(msg.body);
             }
             msg.user = user.uid;
-            Rooms.send(user.roomId,msg)
-        }else{
-            io.emit('message',msg)
+            Rooms.send(user.roomId, msg)
+        } else {
+            io.emit('message', msg)
         }
         console.log(msg)
-    })
+    });
     client.on('disconnect', () => {
         /* … */
         console.log("连接断开")
--- message.js
+++ message.js
@@ -3,7 +3,10 @@ const actionType = {
     leave:'LEAVE',//离开
     talk:'TALK',//消息
     action:'ACTION',//用户操作
-    push:'PUSH'//系统推送
+    push:'PUSH',//系统推送
+    addRebots:'ADDREBOTS'//机器人
+    updateRebots:'UPDATEREBOTS'//机器人
+    getRebots:'GETREBOTS'//机器人
 }
 //消息体
 class MSG {
