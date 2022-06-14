7c09bc53681e8f84c02dad6c1c52eab6ea3ea513
hecongyuan <hecongyuan@hecongyuan.local>
2022-06-14 12:50:37 +0800

    添加注释

--- room.js
+++ room.js
@@ -1,7 +1,7 @@
 /**
  * 房间
  */
-const {MSG,actionType} = require('./message');
+const { MSG, actionType } = require('./message');
 
 class Room {
     constructor(roomId, user, io) {
@@ -11,33 +11,34 @@ class Room {
     }
     sendMsg(msg) {
         msg.roomId = this.roomId;
-        this.io.to(this.roomId).emit('message',msg);
+        this.io.to(this.roomId).emit('message', msg);
     }
-    add(user,client) {
+    add(user, client) {
         let roomId = this.roomId;
         client.join(roomId, (a, b) => {
             let rooms = Object.keys(client.rooms);
             console.log(rooms);
-            user.updateRoom(roomId,client)
+            user.updateRoom(roomId, client)
             this.peoples.push(user);
             console.log(roomId + '新的用户加入', user)
-            this.sendMsg(new MSG(actionType.join,{
-                user:user.uid,
-                peoples:this.mapPeoples()
+                //发送固定的消息格式
+            this.sendMsg(new MSG(actionType.join, {
+                user: user.uid,
+                peoples: this.mapPeoples()
             }));
         })
     }
-    mapPeoples(){
-        return this.peoples.map(user=>user.info)
+    mapPeoples() {
+        return this.peoples.map(user => user.info)
     }
     leave(user) {
         this.peoples.forEach((item, index) => {
             if (user.uid === item.uid) {
                 this.peoples.splice(index, 1);
-                user.client.leave(user.roomId,()=>{
-                    this.sendMsg(new MSG(actionType.leave,{
-                        user:user.uid,
-                        peoples:this.mapPeoples()
+                user.client.leave(user.roomId, () => {
+                    this.sendMsg(new MSG(actionType.leave, {
+                        user: user.uid,
+                        peoples: this.mapPeoples()
                     }))
                 })
             }
